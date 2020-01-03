a9f46c02a5ca016252e60d6278b5c3bf5166937f
Wizna <12302010012@fudan.edu.cn>
2020-01-03 17:46:33 +0100

    generate lyric with a certain format

--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@ import argparse
 import string
 from tqdm import trange
 from transformers import GPT2LMHeadModel
+from itertools import combinations
 
 
 def is_word(word):
@@ -74,23 +75,55 @@ def sample_sequence(model, context, length, n_ctx, tokenizer,
                     top_k=30,
                     top_p=0.0,
                     repetition_penalty=1.0,
-                    device='cpu'):
+                    device='cpu',
+                    repeat_map={},
+                    model_lyric=[]):
     context = torch.tensor(context, dtype=torch.long, device=device)
     context = context.unsqueeze(0)
     generated = context
+
+    with open('word_frequency.txt', 'r', encoding='utf-8') as f:
+        word_descend = f.readline()
+        token_descend = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(word_descend))
+        # print(len(token_descend))
+        token_descend = [x for x in token_descend if x != 100]
+        # print(len(token_descend))
+
+    print(f'the repeat_map:{repeat_map}')
+    index = 2
     with torch.no_grad():
-        for _ in trange(length):
-            inputs = {'input_ids': generated[0][-(n_ctx - 1):].unsqueeze(0)}
-            outputs = model(
-                **inputs)  # Note: we could also use 'past' with GPT-2/Transfo-XL/XLNet (cached hidden-states)
-            next_token_logits = outputs[0][0, -1, :]
-            for id_num in set(generated):
-                next_token_logits[id_num] /= repetition_penalty
-            next_token_logits = next_token_logits / temperature
-            next_token_logits[tokenizer.convert_tokens_to_ids('[UNK]')] = -float('Inf')
-            filtered_logits = top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
-            next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
+        while index < length:
+            if index in repeat_map:
+                start, span = repeat_map[index]
+                generated = torch.cat((generated, generated[0][start:start + span].unsqueeze(0)), dim=1)
+                index += span
+                continue
+            elif model_lyric[index] == 102:
+                generated = torch.cat((generated, torch.tensor([[102]])), dim=1)
+                index += 1
+                continue
+
+            next_token = torch.tensor(data=102)
+            while next_token == 102:
+                inputs = {'input_ids': generated[0][-(n_ctx - 1):].unsqueeze(0)}
+                outputs = model(
+                    **inputs)  # Note: we could also use 'past' with GPT-2/Transfo-XL/XLNet (cached hidden-states)
+                next_token_logits = outputs[0][0, -1, :]
+                for id_values in set(generated):
+                    # print(f"id_num: {id_num}")
+                    for id_num in id_values:
+                        if id_num in token_descend:
+                            frequency_index = token_descend.index(id_num)
+                            next_token_logits[id_num] /= (
+                                repetition_penalty / (frequency_index + 6) * (frequency_index + 5))
+                next_token_logits = next_token_logits / temperature
+                next_token_logits[tokenizer.convert_tokens_to_ids('[UNK]')] = -float('Inf')
+                filtered_logits = top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
+                next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
             generated = torch.cat((generated, next_token.unsqueeze(0)), dim=1)
+            # print(f'the generated:{generated}')
+            print(next_token)
+            index += 1
     return generated.tolist()[0]
 
 
@@ -118,20 +151,57 @@ def fast_sample_sequence(model, context, length, temperature=1.0, top_k=30, top_
 # 通过命令行参数--fast_pattern，指定模式
 def generate(n_ctx, model, context, length, tokenizer, temperature=1, top_k=0, top_p=0.0, repetition_penalty=1.0,
              device='cpu',
-             is_fast_pattern=False):
+             is_fast_pattern=False,
+             repeat_map={},
+             model_lyric=[]):
     if is_fast_pattern:
         return fast_sample_sequence(model, context, length, temperature=temperature, top_k=top_k, top_p=top_p,
                                     device=device)
     else:
         return sample_sequence(model, context, length, n_ctx, tokenizer=tokenizer, temperature=temperature, top_k=top_k,
                                top_p=top_p,
-                               repetition_penalty=repetition_penalty, device=device)
+                               repetition_penalty=repetition_penalty, device=device, repeat_map=repeat_map,
+                               model_lyric=model_lyric)
+
+
+def find_repeating_sequence_from_list(arr):
+    result = {}
+    longest = len(arr) // 2
+    remain = set(range(len(arr) + 1))
+
+    for length in range(longest, 2, -1):
+        pos = [p for p in remain if p + length in remain]
+        for s1, s2 in combinations(pos, 2):
+            if s2 - s1 < length:
+                continue
+            else:
+                if arr[s1:s1 + length] == arr[s2:s2 + length]:
+                    remain -= set(range(s1, s1 + length))
+                    remain -= set(range(s2, s2 + length))
+                    result[s2] = (s1, length)
+
+    return result
+
+
+def read_and_tokenize_text(file_path, tokenizer):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+
+    raw_text = ''.join(data)
+    raw_text = raw_text.replace('\n', ' [SEP] ')
+    print(raw_text)
+    context_tokens = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(raw_text))
+    context_tokens = [tokenizer.convert_tokens_to_ids('[CLS]'),
+                      tokenizer.convert_tokens_to_ids('[MASK]')] + context_tokens
+    print(context_tokens)
+    return context_tokens
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', default='0,1,2,3', type=str, required=False, help='生成设备')
     parser.add_argument('--length', default=-1, type=int, required=False, help='生成长度')
+    parser.add_argument('--original_lyric', default='original_lyric.txt', type=str, required=False, help='原版歌词，模仿句式')
     parser.add_argument('--batch_size', default=1, type=int, required=False, help='生成的batch size')
     parser.add_argument('--nsamples', default=10, type=int, required=False, help='生成几个样本')
     parser.add_argument('--temperature', default=1, type=float, required=False, help='生成温度')
@@ -182,8 +252,10 @@ def main():
             os.makedirs(args.save_samples_path)
         samples_file = open(args.save_samples_path + '/samples.txt', 'w', encoding='utf8')
     while True:
-        raw_text = args.prefix
-        context_tokens = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(raw_text))
+        context_tokens = read_and_tokenize_text(args.prefix, tokenizer=tokenizer)
+        lyric_tokens = read_and_tokenize_text(args.original_lyric, tokenizer=tokenizer)
+        repeat_map = find_repeating_sequence_from_list(lyric_tokens)
+        length = 60 # len(lyric_tokens)
         generated = 0
         for _ in range(nsamples // batch_size):
             out = generate(n_ctx=n_ctx,
@@ -196,7 +268,9 @@ def main():
                            top_k=topk,
                            top_p=topp,
                            repetition_penalty=repetition_penalty,
-                           device=device)
+                           device=device,
+                           repeat_map=repeat_map,
+                           model_lyric=lyric_tokens)
             # for i in range(batch_size):
             generated += 1
             text = tokenizer.convert_ids_to_tokens(out)
