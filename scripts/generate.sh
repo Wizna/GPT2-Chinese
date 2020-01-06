f6a0570d590356939c36eba63d34553f18390683
Wizna <12302010012@fudan.edu.cn>
2020-01-06 03:15:19 +0100

    save scripts settings

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 python generate.py \
   --device 0 \
-  --length 900 \
-  --tokenizer_path cache/vocab_user.txt \
-  --model_path 'C:\\Users\\Ruiming Huang\\Desktop\\chkp8' \
-  --prefix "[CLS][MASK]" \
-  --topp 1.0 \
+  --tokenizer_path cache/vocab_small.txt \
+  --model_path model/model_epoch35 \
+  --topp 0.9 \
+  --topk 8 \
   --temperature 1.0 \
-  --nsamples 3 \
-
-
+  --nsamples 1 \
+  --prefix pre_context.txt \
+  --repetition_penalty 1.1 \
+  --original_lyric original_lyric.txt \
+  --batch_size 1
--- scripts/train.sh
+++ scripts/train.sh
@@ -2,18 +2,15 @@
 python train.py \
   --model_config config/model_config_small.json \
   --tokenized_data_path data/tokenized/ \
-  --tokenizer_path cache/vocab_user.txt \
+  --tokenizer_path cache/vocab_small.txt \
   --raw_data_path data/train.json \
-  --epochs 30 \
+  --epochs 45 \
   --log_step 200 \
   --stride 512 \
   --output_dir model/ \
   --device 0,1,2,3 \
-  --num_pieces 100 \
+  --num_pieces 120 \
   --min_length 20 \
   --batch_size 4 \
-  --segment \
-  --raw \
 
-
-#  --pretrained_model model/model_epoch15
+#   --pretrained_model model/model_epoch37
